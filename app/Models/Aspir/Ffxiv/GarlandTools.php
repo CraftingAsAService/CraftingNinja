c65c31bfd3366f7f86e8cfad6cc94d030fcc181b
Nick Wright <tickthokk@gmail.com>
2019-10-29 20:14:02 -0400

    👽 Incorporating more Garland data

--- app/Models/Aspir/Ffxiv/GarlandTools.php
+++ app/Models/Aspir/Ffxiv/GarlandTools.php
@@ -39,6 +39,67 @@ trait GarlandTools
 		});
 	}
 
+	public function garlandNpcs()
+	{
+		$this->loopGarlandEndpoint('npc', function($data) {
+			if ( ! (isset($data->npc->zoneid) && $data->npc->zoneid))
+				return;
+
+			$this->setData('coordinates', [
+				'zone_id'         => $data->npc->zoneid,
+				'coordinate_id'   => $data->npc->id,
+				'coordinate_type' => 'npc', // See Relation::morphMap in AppServiceProvider
+				'x'               => $data->npc->coords[0] ?? null,
+				'y'               => $data->npc->coords[1] ?? null,
+				'z'               => null,
+				'radius'          => null,
+			]);
+		});
+	}
+
+	public function garlandInstances()
+	{
+		$this->loopGarlandEndpoint('instance', function($data) {
+			$itemIds = [];
+
+			if (isset($data->instance->fights))
+				foreach ($data->instance->fights as $f)
+				{
+					if (isset($f->coffer))
+						$itemIds = array_merge($itemIds, $f->coffer->items);
+
+					foreach ($f->mobs as $mobID)
+						$this->setData('coordinates', [
+							'zone_id'         => $data->instance->id,
+							'coordinate_id'   => $this->translateMobID($mobID),
+							'coordinate_type' => 'npc', // See Relation::morphMap in AppServiceProvider
+							'x'               => null,
+							'y'               => null,
+							'z'               => null,
+							'radius'          => null,
+						]);
+				}
+
+			if (isset($data->instance->rewards))
+				$itemIds = array_merge($itemIds, $data->instance->rewards);
+
+			if (isset($data->instance->coffers))
+				foreach ($data->instance->coffers as $coffer)
+					$itemIds = array_merge($itemIds, $coffer->items);
+
+			foreach ($itemIds as $itemId)
+				$this->setData('coordinates', [
+					'zone_id'         => $data->instance->id,
+					'coordinate_id'   => $itemId,
+					'coordinate_type' => 'item', // See Relation::morphMap in AppServiceProvider
+					'x'               => null,
+					'y'               => null,
+					'z'               => null,
+					'radius'          => null,
+				]);
+		});
+	}
+
 	/**
 	 * Helper Functions
 	 */
